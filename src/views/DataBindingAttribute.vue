683557c6bee6395eae8cad9244bafa1ddce8d38e
HJW <ayyam670@naver.com>
2025-08-28 15:20:23 +0900

    first commit

---
+++ src/views/DataBindingAttribute.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <img v-bind:src="imgSrc" v-bind:width="widthVal" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      imgSrc: "http://vuejs.org/images/logo.png",
+      widthVal: "100px",
+    };
+  },
+};
+</script>
